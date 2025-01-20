2ea3d0afdd162345d926642d736f44f36b8496d5
Santiago Tomas Nadal <60269285+AstarothNephilim@users.noreply.github.com>
2025-01-20 17:15:48 +0100

    Starting process of adding users

---
+++ app/services/database_service.py
@@ -0,0 +1,14 @@
+from flask import request, current_app
+'''
+
+Lista de operaciones que quiero hacer en la base de datos:
+
+Fetch dashboard data for a company : @ns_dashboard.route('/empresa/<string:empresa_id>/dashboard')
+
+
+
+
+'''
+
+class DatabaseOperation:
+    def __init__(self, )
